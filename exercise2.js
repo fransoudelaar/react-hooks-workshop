b35b25bab07ae45cfb267b63f762f730dec62f79
Frans Oudelaar <f.oudelaar@wearetriple.com>
2019-05-15 15:57:32 +0200

    add exercise 2

---
+++ exercise2.js
@@ -0,0 +1,41 @@
+/* 
+*
+* Exercise 2:
+* Add the FriendStatus class from below to your project and rewrite it using Hooks
+*
+*/
+
+class FriendStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOnline: null };
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+  }
+
+  componentDidMount() {
+    ChatAPI.subscribeToFriendStatus(
+      this.props.friend.id,
+      this.handleStatusChange
+    );
+  }
+
+  componentWillUnmount() {
+    ChatAPI.unsubscribeFromFriendStatus(
+      this.props.friend.id,
+      this.handleStatusChange
+    );
+  }
+
+  handleStatusChange(status) {
+    this.setState({
+      isOnline: status.isOnline
+    });
+  }
+
+  render() {
+    if (this.state.isOnline === null) {
+      return 'Loading...';
+    }
+    return this.state.isOnline ? 'Online' : 'Offline';
+  }
+}
